a89db118f8faed367c6c450d7996a9475ceb2183
Alex <66566820+AlexMariacho@users.noreply.github.com>
2021-10-15 13:01:13 +0400

    Добавлены анимации на экране отображения девочки

--- Clicker/Assets/App/Scripts/UiControllers/GameScreen/MinerInformationPanel/MinerInformationPanelAnimation.cs
+++ Clicker/Assets/App/Scripts/UiControllers/GameScreen/MinerInformationPanel/MinerInformationPanelAnimation.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using DG.Tweening;
 using Sirenix.OdinInspector;
 using UnityEngine;
 
@@ -7,6 +8,10 @@ namespace App.Scripts.UiControllers.GameScreen.MinerInformationPanel
     public class MinerInformationPanelAnimation : MonoBehaviour
     {
         [SerializeField] private MinerInformationPanelUiController _panelUiController;
+
+        [Title("Настройки анимации")] 
+        [SerializeField] private int _distanceSideHideElementPixels = 800;
+        [SerializeField] private float _animationTime = 1;
         
         [Title("Элементы панели")] 
         [SerializeField] private GameObject _rootPanel;
@@ -28,6 +33,29 @@ namespace App.Scripts.UiControllers.GameScreen.MinerInformationPanel
 
         private void PlayShowAnimation()
         {
+            _backgroundBlockRaycast.alpha = 0;
+
+            DOTween.To(
+                ()=> _backgroundBlockRaycast.alpha, 
+                x=> _backgroundBlockRaycast.alpha = x, 
+                1f, _animationTime);
+
+            _informationPanel.transform.position = new Vector3(
+                _informationPanel.transform.position.x + _distanceSideHideElementPixels,
+                _informationPanel.transform.position.y,
+                _informationPanel.transform.position.z);
+            _informationPanel.transform.DOMove(new Vector3(
+                _informationPanel.transform.position.x - _distanceSideHideElementPixels,
+                _informationPanel.transform.position.y), _animationTime);
+            
+            _minerRootPosition.transform.position = new Vector3(
+                _minerRootPosition.transform.position.x - _distanceSideHideElementPixels,
+                _minerRootPosition.transform.position.y,
+                _minerRootPosition.transform.position.z);
+            _minerRootPosition.transform.DOMove(new Vector3(
+                _minerRootPosition.transform.position.x + _distanceSideHideElementPixels,
+                _minerRootPosition.transform.position.y), _animationTime);
+            
             _rootPanel.SetActive(true);
         }
 
