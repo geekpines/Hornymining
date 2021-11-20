a252cd422ab5762b1cb008d81d8455ebb8dc868f
Belich <43599300+Chopperl@users.noreply.github.com>
2021-11-20 23:38:00 +0400

    Revert "Revert "Изменен визуал, некоторые доработки""
    This reverts commit f3a1335c572cfc8a25fbb92ec40c3025718a6037.

--- Clicker/Assets/App/Scripts/UiControllers/MainCanvasUiController.cs
+++ Clicker/Assets/App/Scripts/UiControllers/MainCanvasUiController.cs
@@ -9,20 +9,36 @@ namespace App.Scripts.UiControllers
         [SerializeField] private GameObject _gameScreen;
         [SerializeField] private GameObject _rouletteScreen;
         [SerializeField] private GameObject _shopScreen;
+        [SerializeField] private GameObject _stockPanel;
+        [SerializeField] private GameObject _upgradePanel;
 
         [SerializeField] private Button _openRouletteScreenButton;
         [SerializeField] private Button _backRouletteScreenButton;
 
-        [SerializeField] private Button _openShopScreenButton;
-        [SerializeField] private Button _backShopScreenButton;
+        [SerializeField] private Button _openStockButton;
+        [SerializeField] private Button _openStockButton1;
+        [SerializeField] private Button _backStockButton;
+
+        [SerializeField] private Button _openUpgradeButton;
+        [SerializeField] private Button _openUpgradeButton1;
+        [SerializeField] private Button _backUpgradeButton;
 
         private void OnEnable()
         {
             _openRouletteScreenButton.onClick.AddListener(ShowRouletteScreen);
             _backRouletteScreenButton.onClick.AddListener(ShowGameScreen);
 
-            _openShopScreenButton.onClick.AddListener(ShowShopScreen);
-            _backShopScreenButton.onClick.AddListener(ShowGameScreen);
+            //открытие биржи и апгрейда из Gamescreen
+            _openStockButton.onClick.AddListener(ShowStockScreen);
+            _openUpgradeButton.onClick.AddListener(ShowUpgradeScreen);
+
+            //открытие биржи и апгрейда из Shopscreen
+            _openStockButton1.onClick.AddListener(ShowStockScreen);
+            _openUpgradeButton1.onClick.AddListener(ShowUpgradeScreen);
+
+            //закрытие биржи и апгрейда
+            _backStockButton.onClick.AddListener(ShowGameScreen);
+            _backUpgradeButton.onClick.AddListener(ShowGameScreen);
         }
 
         private void OnDisable()
@@ -42,13 +58,30 @@ namespace App.Scripts.UiControllers
             _gameScreen.SetActive(true);
             _rouletteScreen.SetActive(false);
             _shopScreen.SetActive(false);
+            _openStockButton.gameObject.SetActive(true);
+            _openUpgradeButton.gameObject.SetActive(true);
         }
 
-        private void ShowShopScreen()
+        private void ShowStockScreen()
         {
-            _gameScreen.SetActive(false);
             _shopScreen.SetActive(true);
+            _stockPanel.SetActive(true);
+            _upgradePanel.SetActive(false);
+
+            _openStockButton.gameObject.SetActive(false);
+            _openUpgradeButton.gameObject.SetActive(false);
+            
         }
-        
+
+        private void ShowUpgradeScreen()
+        {
+            _shopScreen.SetActive(true);
+            _upgradePanel.SetActive(true);
+            _stockPanel.SetActive(false);
+
+            _openStockButton.gameObject.SetActive(false);
+            _openUpgradeButton.gameObject.SetActive(false);
+        }
+
     }
 }
