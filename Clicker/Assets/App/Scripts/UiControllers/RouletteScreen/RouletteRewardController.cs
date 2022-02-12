c87d964abd8b33ad2659e785b55a1f807e5808d7
Belich <43599300+Chopperl@users.noreply.github.com>
2022-02-12 19:35:19 +0400

    Продолжаем изменять roulette

--- Clicker/Assets/App/Scripts/UiControllers/RouletteScreen/RouletteRewardController.cs
+++ Clicker/Assets/App/Scripts/UiControllers/RouletteScreen/RouletteRewardController.cs
@@ -3,13 +3,15 @@ using App.Scripts.Gameplay.CoreGameplay.Player;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Zenject;
 
 public class RouletteRewardController : MonoBehaviour
 {
     [SerializeField] private GameObject _rewardMinerWindow;
     [SerializeField] private GameObject _wheelContainer;
-    [SerializeField] private Transform RewardMinerPosition;
+    [SerializeField] private Transform _rewardMinerPosition;
+    [SerializeField] private Button _backButton;
 
     private PlayerProfile _playerProfile;
 
@@ -22,14 +24,21 @@ public class RouletteRewardController : MonoBehaviour
     void Start()
     {
         _playerProfile.OnAllMinersCountChanged += AddMinerToRewardPosition;
+        _backButton.onClick.AddListener(Back);
     }
 
     void AddMinerToRewardPosition(Miner miner)
     {        
         _wheelContainer.SetActive(false);
         _rewardMinerWindow.SetActive(true);
-        Instantiate(miner.Configuration.Visual, RewardMinerPosition);
+        var visualContext = Instantiate(miner.Configuration.Visual, _rewardMinerPosition);
+        visualContext.transform.localScale = new Vector3(12, 12, 12);
+        visualContext.transform.localPosition = new Vector3(0, -137, 0);
     }
 
-    
+    private void Back()
+    {
+        _wheelContainer.SetActive(true);
+        _rewardMinerWindow.SetActive(false);
+    }
 }
